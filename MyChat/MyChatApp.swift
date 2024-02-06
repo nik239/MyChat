88c133e0b06560f54a362862f8e221894013044c
nik239 <116445208+nik239@users.noreply.github.com>
2024-02-05 22:38:24 -0500

    first commit

---
+++ MyChat/MyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatApp.swift
+//  MyChat
+//
+//  Created by Nikita Ivanov on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
