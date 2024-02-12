d2567afd18435205cc9292b8f5e2e245922df03e
nik239 <116445208+nik239@users.noreply.github.com>
2024-02-11 23:12:27 -0500

    Adding some test code

--- MyChat/MyChatApp.swift
+++ MyChat/MyChatApp.swift
@@ -7,12 +7,13 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-    
+    Auth.auth().useEmulator(withHost:"localhost", port:9099)
     return true
   }
 }
---
+++ MyChatTests/AuthEmailPasswordTests.swift
@@ -0,0 +1,45 @@
+//
+//  MyChatTests.swift
+//  MyChatTests
+//
+//  Created by Nikita Ivanov on 11/02/2024.
+//
+
+import XCTest
+@testable import MyChat
+
+//boot up Firebase emulator before running
+final class AuthEmailPasswordTests: XCTestCase {
+  private var authModel: AuthViewModel!
+  
+  private let timeout: TimeInterval = 0.01
+  //private var expectation: XCTestExpectation!
+  
+  @MainActor
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    if authModel == nil {
+      authModel = AuthViewModel()
+    }
+    authModel.signOut()
+  }
+
+  
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
+  
+  @MainActor
+  func test_signup() async throws {
+    //given
+    authModel.email = "test@mail.com"
+    authModel.password = "testpassword"
+    authModel.confirmPassword = "testpassword"
+    
+    //when
+    let success = await authModel.signUpWithEmailPassword()
+    
+    //then
+    XCTAssertTrue(success)
+  }
+}
